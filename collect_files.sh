929e187fd3e19e6193b977bb9bfbfb3b7d8985f8
Anna Varnavskaia <rrr26363@gmail.com>
2025-05-01 21:25:24 +0300

    Добавила копирование файлов из вложенных папок

--- collect_files.sh
+++ collect_files.sh
@@ -2,3 +2,17 @@
 
 input_dir="$1"
 output_dir="$2"
+if [ "$#" -ne 2 ]; then
+  echo "Использование: $0 /путь/к/входной_директории /путь/к/выходной_директории"
+  exit 1
+fi
+if [ ! -d "$input_dir" ]; then
+  echo "Ошибка: входная директория '$input_dir' не существует."
+  exit 1
+fi
+mkdir -p "$output_dir"
+find "$input_dir" -type f
+find "$input_dir" -type f | while read -r file; do
+  filename=$(basename "$file")
+  cp "$file" "$output_dir/$filename"
+done
