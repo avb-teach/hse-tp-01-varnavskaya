4b8bfa360d1a13396d850d3cbf5cda821c43674f
Anna Varnavskaia <rrr26363@gmail.com>
2025-05-01 22:12:50 +0300

    student_branch

--- collect_files.sh
+++ collect_files.sh
@@ -3,11 +3,9 @@
 input_dir="$1"
 output_dir="$2"
 if [ "$#" -ne 2 ]; then
-  echo "Использование: $0 /путь/к/входной_директории /путь/к/выходной_директории"
   exit 1
 fi
 if [ ! -d "$input_dir" ]; then
-  echo "Ошибка: входная директория '$input_dir' не существует."
   exit 1
 fi
 mkdir -p "$output_dir"
